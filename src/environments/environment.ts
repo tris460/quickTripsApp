290d941632c620f023edf679705edd390cba7285
Beatriz Martinez <trism@gmail.com>
2021-10-17 16:55:45 -0500

    Add firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyARLfOmYW3hVJ7NlMM390TutT_eyaVrIZQ",
+    authDomain: "fir-test-c56d2.firebaseapp.com",
+    projectId: "fir-test-c56d2",
+    storageBucket: "fir-test-c56d2.appspot.com",
+    messagingSenderId: "935425201559",
+    appId: "1:935425201559:web:7a30cdf5012a60e34860d2"
+  }
 };
 
 /*
