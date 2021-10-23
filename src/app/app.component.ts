0eb10eeb93ff02b4e89fb93d0f870a75646ad17b
Bety Martinez <trism460@gmail.com>
2021-10-23 17:19:47 -0500

    Show in console the products registered

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,20 @@ import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 export class AppComponent {
   title = 'firebaseProyect';
   productList: AngularFireList<any>;
+  product: Array<any>;
 
   constructor(public firebase:AngularFireDatabase){
+    // Crea una tabla en la base de datos
     this.productList = this.firebase.list('product');
+    this.product = [];
+    // Obtiene los productos que haya registrados en la BD
+    this.productList.snapshotChanges().subscribe(item => {
+      item.forEach(producto => {
+        let x = producto.payload.toJSON();
+        this.product.push(x);
+      });
+    });
+    console.log('Products:', this.product);
   }
 
   addProduct() {
