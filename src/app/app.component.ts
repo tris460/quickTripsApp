eef5fcb44a8d4942127af2772ea27e5da8d9eca2
Bety Martinez <trism460@gmail.com>
2021-11-03 12:54:10 -0600

    Change login as a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -7,67 +6,7 @@ import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firebaseProyect';
-  username: string;
-  email: string;
-  password: string;
-  repeatPassword: string;
-  userList: AngularFireList<any>;
+  title = 'firebaseProyect';  
 
-  constructor(public firebase:AngularFireDatabase){
-    this.userList = this.firebase.list('user');
-    this.username = '';
-    this.email = '';
-    this.password = '';
-    this.repeatPassword = '';
-  }
-
-  addUser() {
-    if(this.validateFieldsSignIn()) {
-      this.userList.push({
-        username: this.username,
-        email: this.email,
-        password: this.password
-      });
-      alert('User added correctly');
-      this.clearForm();
-    } else {
-      alert('You need to complete each field');
-    }
-  }
-  clearForm() {
-    this.username = '';
-    this.email = '';
-    this.password = '';
-    this.repeatPassword = '';
-  }
-  validateFieldsSignIn() {
-    if(this.username === '' || this.email === '' || this.password === '' || this.repeatPassword === '') {
-      return false;
-    } else {
-      return true;
-    }
-  }
-
-  // Styes
-  changePositionSignUp() {
-    const signUp = document.getElementById('sing-up-front') || document.createElement('div');
-    const logIn = document.getElementById('log-in-front') || document.createElement('div');
-    signUp.style.zIndex = '0';
-    logIn.style.zIndex = '1';
-    signUp.style.transition = 'all 2s ease';
-    logIn.style.transition = 'all 2s ease';
-    signUp.style.transform = 'translate(100%, 0%)';
-    logIn.style.transform = 'translate(100%, 0%)';
-  }
-  changePositionLogIn() {
-    const signUp = document.getElementById('sing-up-front') || document.createElement('div');
-    const logIn = document.getElementById('log-in-front') || document.createElement('div');
-    signUp.style.zIndex = '1';
-    logIn.style.zIndex = '0';
-    signUp.style.transition = 'all 2s ease';
-    logIn.style.transition = 'all 2s ease';
-    signUp.style.transform = 'translate(0%, 0%)';
-    logIn.style.transform = 'translate(0%, 0%)';
-  }
+  constructor(){ }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 import { AngularFireModule } from 'angularfire2';
@@ -8,14 +8,17 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { environment } from '../environments/environment';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule,
     AngularFireDatabaseModule,
     AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
