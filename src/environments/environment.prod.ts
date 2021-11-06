fe4c7ecd1da9dc948766ecc2bd38bbe967c4385c
Bety Martinez <trism460@gmail.com>
2021-11-06 09:39:01 -0600

    Add firebaseConfig

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyARLfOmYW3hVJ7NlMM390TutT_eyaVrIZQ",
+    authDomain: "fir-test-c56d2.firebaseapp.com",
+    projectId: "fir-test-c56d2",
+    storageBucket: "fir-test-c56d2.appspot.com",
+    messagingSenderId: "935425201559",
+    appId: "1:935425201559:web:7a30cdf5012a60e34860d2"
+  }
 };
