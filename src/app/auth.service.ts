967ff6e039c31470f7ac95e2abe172c8eed1848f
Bety Martinez <trism460@gmail.com>
2021-11-20 19:23:32 -0600

    Delete unused functions/Add routing for Login

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { auth } from 'firebase/app';
 import { AngularFireAuth } from 'angularfire2/auth'; //"@angulat/fire/auth"
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public afAuth: AngularFireAuth) { }
+  constructor(public afAuth: AngularFireAuth, private router: Router) { }
 
   FacebookAuth() {
     return this.AuthLogin(new auth.FacebookAuthProvider())
@@ -16,7 +17,7 @@ export class AuthService {
     return this.afAuth.auth.signInWithPopup(provider)
     .then((result)=>{
       console.log('Logged in succesfull');
-      console.log(result);
+      this.router.navigateByUrl('/map');
     })
     .catch((error)=> {
       console.log(error);
