fe99adeadff307a99fb214ae5aabf7d9d5898366
Bety Martinez <trism460@gmail.com>
2021-12-01 22:48:11 -0600

    Show data in table

--- src/app/travels/travels.component.ts
+++ src/app/travels/travels.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AngularFireList, AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-travels',
@@ -7,12 +8,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./travels.component.scss']
 })
 export class TravelsComponent implements OnInit {
+  travelList: AngularFireList<any>;
+  travel: Array<any>;
 
-  constructor(private router: Router) {
+  constructor(public firebase:AngularFireDatabase, private router: Router) {
     const userInfo = localStorage.getItem('loggedUser');
     if(!userInfo){
       this.router.navigateByUrl('/login')
     }
+
+    this.travelList = this.firebase.list('travel');
+    this.travel = [];
+
+    this.travelList.snapshotChanges().subscribe(item => {
+      this.travel = [];
+      item.forEach(t => {
+        let x = t.payload.toJSON();
+        this.travel.push(x);
+        console.log(x)
+      });
+    });
   }
 
   ngOnInit(): void {
